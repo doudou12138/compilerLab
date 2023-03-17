989bf8cf1cd1cebec1569866fed15a4793d5cae3
tracer-compiler2022 <compilers@software.nju.edu.cb>
2023-03-17 04:00:33 -0700

    > make 211250173 doudou Linux ubuntu 5.15.0-67-generic #74~20.04.1-Ubuntu SMP Wed Feb 22 14:52:34 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  04:00:33 up  6:59,  1 user,  load average: 0.42, 0.40, 0.30 31a6f1ac8b1c13eaba840616d04b45f3de9dc7c9

--- src/Main.java
+++ src/Main.java
@@ -12,29 +12,29 @@ public class Main {
         CharStream input = CharStreams.fromFileName(source);
         SysYLexer sysYLexer = new SysYLexer(input);
 
-        MyErrorListener myErrorListener=new MyErrorListener();
+        MyErrorListener myErrorListener = new MyErrorListener();
         sysYLexer.removeErrorListeners();
         sysYLexer.addErrorListener(myErrorListener);
 
         List<? extends Token> tokens = sysYLexer.getAllTokens();
-        if(myErrorListener.hasError()){
+        if (myErrorListener.hasError()) {
             myErrorListener.changeStatu(false);
-            return;
-        }
-        for (Token i : tokens) {
-            int type_n = i.getType();
-            if (type_n == 34) {
-                String tokenText = i.getText();
-                if (tokenText.charAt(0) == '0') {
-                    if ((tokenText.charAt(1) - 'x' == 0) || (tokenText.charAt(1) - 'X' == 0)) {
-                        tokenText = String.valueOf(Integer.parseInt(tokenText.substring(2), 16));
-                    } else {
-                        tokenText = String.valueOf(Integer.parseInt(tokenText.substring(1), 8));
+        } else {
+            for (Token i : tokens) {
+                int type_n = i.getType();
+                if (type_n == 34) {
+                    String tokenText = i.getText();
+                    if (tokenText.charAt(0) == '0') {
+                        if ((tokenText.charAt(1) - 'x' == 0) || (tokenText.charAt(1) - 'X' == 0)) {
+                            tokenText = String.valueOf(Integer.parseInt(tokenText.substring(2), 16));
+                        } else {
+                            tokenText = String.valueOf(Integer.parseInt(tokenText.substring(1), 8));
+                        }
                     }
+                    System.err.println(SysYLexer.ruleNames[type_n - 1] + " " + tokenText + " at Line " + i.getLine() + ".");
+                } else {
+                    System.err.println(SysYLexer.ruleNames[type_n - 1] + " " + i.getText() + " at Line " + i.getLine() + ".");
                 }
-                System.err.println(SysYLexer.ruleNames[type_n - 1] + " " + tokenText + " at Line " + i.getLine() + ".");
-            } else {
-                System.err.println(SysYLexer.ruleNames[type_n - 1] + " " + i.getText() + " at Line " + i.getLine() + ".");
             }
         }
     }
