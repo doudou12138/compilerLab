bfa6c5268e0beaa29a68881c7744c1e06c7c5070
tracer-compiler2022 <compilers@software.nju.edu.cb>
2023-03-17 03:27:05 -0700

    > make 211250173 doudou Linux ubuntu 5.15.0-67-generic #74~20.04.1-Ubuntu SMP Wed Feb 22 14:52:34 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  03:27:05 up  6:25,  1 user,  load average: 0.05, 0.06, 0.15 15a24bbbbe606d815bc74597476d05348e32f95

--- src/MyErrorListener.java
+++ src/MyErrorListener.java
@@ -2,7 +2,7 @@ import org.antlr.v4.runtime.BaseErrorListener;
 
 
 public class MyErrorListener extends BaseErrorListener {
-    private boolean hasError;
+    private boolean hasError = false;
     @Override
     public void syntaxError(org.antlr.v4.runtime.Recognizer<?,?> recognizer, java.lang.Object offendingSymbol, int line,
                             int charPositionInLine, java.lang.String msg, org.antlr.v4.runtime.RecognitionException e) {
