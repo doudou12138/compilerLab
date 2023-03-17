72d747039fdee69315b32d5c5d0f412a786807ae
tracer-compiler2022 <compilers@software.nju.edu.cb>
2023-03-17 02:40:01 -0700

    > make 211250173 doudou Linux ubuntu 5.15.0-67-generic #74~20.04.1-Ubuntu SMP Wed Feb 22 14:52:34 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  02:40:01 up  5:38,  1 user,  load average: 0.38, 0.26, 0.27 f5b3eea4ed2c1932e1b9d7984a108aad7c40381c

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.misc.ParseCancellationException;
 
 import java.io.IOException;
 import java.util.List;
 
-public class Main
-{    
+public class Main {
     public static void main(String[] args) throws IOException {
         if (args.length < 1) {
             System.err.println("input path is required");
@@ -13,26 +13,30 @@ public class Main
         CharStream input = CharStreams.fromFileName(source);
         SysYLexer sysYLexer = new SysYLexer(input);
 
-        MyErrorListener myErrorListener = new MyErrorListener();
-
         sysYLexer.removeErrorListeners();
-        sysYLexer.addErrorListener(myErrorListener);
-        List<? extends Token> tokens = sysYLexer.getAllTokens();
-        for(Token i:tokens) {
-            int type_n = i.getType();
-            if (type_n == 34) {
-                String tokenText = i.getText();
-                if (tokenText.charAt(0) == '0') {
-                    if ((tokenText.charAt(1) - 'x' == 0) || (tokenText.charAt(1) - 'X' == 0)) {
-                        tokenText = String.valueOf(Integer.parseInt(tokenText.substring(2), 16));
-                    } else {
-                        tokenText = String.valueOf(Integer.parseInt(tokenText.substring(1), 8));
+        sysYLexer.addErrorListener(new MyErrorListener());
+
+        try {
+            List<? extends Token> tokens = sysYLexer.getAllTokens();
+            for (Token i : tokens) {
+                int type_n = i.getType();
+                if (type_n == 34) {
+                    String tokenText = i.getText();
+                    if (tokenText.charAt(0) == '0') {
+                        if ((tokenText.charAt(1) - 'x' == 0) || (tokenText.charAt(1) - 'X' == 0)) {
+                            tokenText = String.valueOf(Integer.parseInt(tokenText.substring(2), 16));
+                        } else {
+                            tokenText = String.valueOf(Integer.parseInt(tokenText.substring(1), 8));
+                        }
                     }
+                    System.err.println(SysYLexer.ruleNames[type_n - 1] + " " + tokenText + " at Line " + i.getLine() + ".");
+                } else {
+                    System.err.println(SysYLexer.ruleNames[type_n - 1] + " " + i.getText() + " at Line " + i.getLine() + ".");
                 }
-                System.err.println(SysYLexer.ruleNames[type_n-1] + " " + tokenText + " at Line " + i.getLine() + ".");
-            } else {
-                System.err.println(SysYLexer.ruleNames[type_n-1] + " " + i.getText() + " at Line " + i.getLine() + ".");
             }
+        } catch (ParseCancellationException e) {
+            System.err.println(e.getMessage());
         }
+
     }
 }
